8c0266d888a8786326b4188ef2d421454afc37a1
Lina Kulakova <kulakova.lina@gmail.com>
2012-10-14 20:54:25 +0400

    use template parameters for operator+

--- matrix_addition/main.cpp
+++ matrix_addition/main.cpp
@@ -3,59 +3,59 @@
 
 #include "my_matrix.h"
 
+
 int main ()
 {
   typedef double NumericType;
 
   // setup:
-  size_t iters = 23;
-  size_t s[] = {  10,   20,   30,   40,   50,  60,  70,  80,  90,
-                 100,  200,  300,  400,  500, 600, 700, 800, 900,
-                1000, 2000, 3000, 4000, 5000};
-  size_t k_max[] = { 500000, 200000, 90000, 50000, 30000, 20000, 15000, 12000, 10000,
-                       7000,   1000,   300,   200,   100,    80,    60,    50,    40,
-                         30,      8,     4,     2,     1};
+  constexpr size_t s[] = {  10,   20,   30,   40,   50,  60,  70,  80,  90,
+                           100,  200,  300,  400,  500, 600, 700, 800, 900,
+                          1000, 2000, 3000, 4000, 5000};
+  constexpr size_t k_max[] = { 1000000, 400000, 90000, 50000, 30000, 20000, 15000, 12000, 10000,
+                                 7000,   1000,   300,   200,   100,    80,    60,    50,    40,
+                                   30,      8,     4,     2,     1};
   size_t k;
   double t;
-  for (size_t it = 0; it < iters; ++it)
+  const size_t it = 22;  // it < 23
+
+  my_matrix<NumericType> m1(s[it], s[it]);
+  my_matrix<NumericType> m2(s[it], s[it]);
+  my_matrix<NumericType> m3(s[it], s[it]);
+  my_matrix<NumericType> m4(s[it], s[it]);
+  my_matrix<NumericType> m5(s[it], s[it]);
+
+  /* fill m1, ..., m5 with values here */
+  for (size_t i = 0; i < s[it]; i++)
+    for (size_t j = 0; j < s[it]; j++)
+      {
+        m2(i, j) = (i + j) / 2.0;
+        m3(i, j) = (i + j) / 3.0;
+        m4(i, j) = (i + j) / 4.0;
+        m5(i, j) = (i + j) / 5.0;
+      }
+
+  cout << "size = " << s[it] << endl;
+  t = clock ();
+  for (k = 0; k < k_max[it]; ++k)
     {
-      my_matrix<NumericType> m1(s[it], s[it]);
-      my_matrix<NumericType> m2(s[it], s[it]);
-      my_matrix<NumericType> m3(s[it], s[it]);
-      my_matrix<NumericType> m4(s[it], s[it]);
-      my_matrix<NumericType> m5(s[it], s[it]);
-
-      /* fill m1, ..., m5 with values here */
-      for (size_t i = 0; i < s[it]; i++)
-        for (size_t j = 0; j < s[it]; j++)
-          {
-            m2(i, j) = (i + j) / 2.0;
-            m3(i, j) = (i + j) / 3.0;
-            m4(i, j) = (i + j) / 4.0;
-            m5(i, j) = (i + j) / 5.0;
-          }
-
-      cout << "size = " << s[it] << endl;
-      t = clock ();
-      for (k = 0; k < k_max[it]; ++k)
-        {
-          for (size_t i = 0; i < s[it]; ++i)
-            for (size_t j = 0; j < s[it]; ++j)
-              m1(i,j) = m2(i,j) + m3(i,j) + m4(i,j) + m5(i,j);
-        }
-      t = clock () - t;
-//      cout << m1 << endl;
-      cout << "Elapsed time ('for' loop): " << t / CLOCKS_PER_SEC << endl;
-
-      t = clock ();
-      for (k = 0; k < k_max[it]; ++k)
-        m1 = m2 + m3 + m4 + m5;
-      t = clock () - t;
-//      cout << m1 << endl;
-      cout << "Elapsed time (my_matrix):  " << t / CLOCKS_PER_SEC << endl;
-
-      cout << endl;
+      for (size_t i = 0; i < s[it]; ++i)
+        for (size_t j = 0; j < s[it]; ++j)
+          m1(i,j) = m2(i,j) + m3(i,j) + m4(i,j) + m5(i,j);
     }
+  t = clock () - t;
+//  cout << m1 << endl;
+  cout << "Elapsed time ('for' loop): " << t / CLOCKS_PER_SEC << endl;
+
+  const size_t matrix_size = s[it] * s[it];
+  t = clock ();
+  for (k = 0; k < k_max[it]; ++k)
+    m1.operator =<4, matrix_size> (m2.operator+<4> (m3) + m4 + m5);
+  t = clock () - t;
+//  cout << m1 << endl;
+  cout << "Elapsed time (my_matrix):  " << t / CLOCKS_PER_SEC << endl;
+
+  cout << endl;
 
   return 0;
 }
--- matrix_addition/my_matrix.h
+++ matrix_addition/my_matrix.h
@@ -13,6 +13,8 @@ using std::ostream;
 using std::size_t;
 using std::vector;
 
+template <typename T, size_t size>
+class my_matrix_temporary;
 
 ////////////////////////////////////////////////
 //           class my_matrix
@@ -25,19 +27,8 @@ public:
   size_t n_rows;     // number of rows
   size_t n_cols;     // number of columns
   T *data;
-  vector<T*> summands;
 
 public:
-  my_matrix (const my_matrix<T> &a, const my_matrix<T> &b)
-  {
-    if (b.n_cols != a.n_cols || b.n_rows != a.n_rows)
-      abort ();
-
-    data = 0;
-    summands.push_back (a.data);
-    summands.push_back (b.data);
-  }
-
   my_matrix (size_t n_rows_arg, size_t n_cols_arg)
     : n_rows (n_rows_arg), n_cols (n_cols_arg)
   {
@@ -64,25 +55,21 @@ public:
     return data[i * n_cols + j];
   }
 
-  my_matrix<T> &operator= (const my_matrix<T> &&a)
+  template<size_t size>
+  my_matrix_temporary<T, size> operator+ (const my_matrix<T> &b)
   {
-    if (n_cols * n_rows < 40000)
+    return my_matrix_temporary<T, size> (*this, b);
+  }
+
+  template<size_t size, size_t matrix_size>
+  my_matrix<T> &operator= (const my_matrix_temporary<T, size> &a)
+  {
+    for (size_t i = 0; i < matrix_size; ++i)
       {
-        for (size_t i = 0; i < n_rows * n_cols; ++i)
-          data[i] = (a.summands[0])[i];
-        for (size_t k = 1; k < a.summands.size (); ++k)
-          for (size_t i = 0; i < n_rows * n_cols; ++i)
-            data[i] += (a.summands[k])[i];
-      }
-    else
-      {
-        for (size_t i = 0; i < n_rows * n_cols; ++i)
-          {
-            T s = 0;
-            for (size_t k = 0; k < a.summands.size (); ++k)
-              s += (a.summands[k])[i];
-            data[i] = s;
-          }
+        T s = 0;
+        for (size_t k = 0; k < size; ++k)
+          s += (a.summands[k])[i];
+        data[i] = s;
       }
     return *this;
   }
@@ -102,16 +89,31 @@ public:
 
 
 
+////////////////////////////////////////////////
+//           class my_matrix_temporary
+////////////////////////////////////////////////
 
-template <typename T> inline
-my_matrix<T> operator+ (const my_matrix<T> &a, const my_matrix<T> &b)
+template<typename T, size_t size>
+class my_matrix_temporary
 {
-  return my_matrix<T> (a, b);
-}
+public:
+  T *summands[size];
+  size_t current_size;
 
-template <typename T> inline
-my_matrix<T> operator+ (my_matrix<T> &&a, const my_matrix<T> &b)
-{
-  a.summands.push_back (b.data);
-  return a;
-}
+public:
+  my_matrix_temporary (const my_matrix<T> &a, const my_matrix<T> &b)
+  {
+    if (b.n_cols != a.n_cols || b.n_rows != a.n_rows)
+      abort ();
+
+    summands[0] = a.data;
+    summands[1] = b.data;
+    current_size = 2;
+  }
+
+  my_matrix_temporary<T, size> operator+ (const my_matrix<T> &b)
+  {
+    summands[current_size++] = b.data;
+    return *this;
+  }
+};
